cc2b6f75488153bf1e22bb7e825410ef46b76a57
Igor Soarez <igorsoarez@gmail.com>
2013-03-29 17:37:20 +0000

    Add logs

--- wss/server.js
+++ wss/server.js
@@ -5,11 +5,16 @@ stream.setEncoding('utf8');
 stream.resume();
 
 var http = require('http');
+var url = require('url');
 var server = http.createServer(handler);
 
 var staticDirectory = path.resolve('..', 'public');
 var staticServer = new (require('node-static').Server)(staticDirectory);
 function handler(req, res) {
+  var pathname = url.parse(req.url).pathname;
+  console.log('Serving %s with %s',
+      pathname, path.resolve(staticDirectory, pathname));
+
   req.on('end', serveStatic);
 
   function serveStatic() {
@@ -26,8 +31,15 @@ function session(connection) {
   function onData(data) { connection.send(data); }
   
   connection.on('close', onClose);
-  function onClose() { stream.removeListener('data', onData); }
+  function onClose() {
+    stream.removeListener('data', onData);
+    console.log('WebSocket connection closed');
+  }
+
+  console.log('WebSocket connection open.');
 }
 
-server.listen(8800);
+var port = process.env.PORT || 8800;
+server.listen(port);
+console.log('Listening on port %s', port);
 
