406f8782456840907156e2d9b63b73b82c928489
Igor Soarez <igorsoarez@gmail.com>
2013-03-29 16:56:26 +0000

    Simplify wss

--- wss/server.js
+++ wss/server.js
@@ -1,26 +1,10 @@
 var path = require('path');
-var inpathSync = require('inpath').sync;
-var sudoPath = inpathSync('sudo');
 
-var path = require('path');
-var executablePath = path.resolve('test.js');
-
-var spawn = require('child_process').spawn;
-var stdio = [ process.stdin, 'pipe', process.stderr];
-var logan = spawn(sudoPath, [executablePath], { stdio: stdio });
-logan.stdout.setEncoding('utf8');
-
-var interval;
-function requestData() { logan.kill('SIGUSR1'); }
-
-process.on('SIGINT', onKill);
-function onKill() {
-  logan.kill();
-  process.exit(0);
-}
+var stream = process.stdin;
+stream.setEncoding('utf8');
+stream.resume();
 
 var http = require('http');
-var url = require('url');
 var server = http.createServer(handler);
 
 var staticDirectory = path.resolve('..', 'public');
@@ -38,22 +22,11 @@ var wss = new WebSocketServer({ server: server });
 wss.on('connection', session);
 
 function session(connection) {
-  logan.stdout.on('data', onData);
-  connection.on('close', onClose);
-
-  if (!interval)
-    interval = setInterval(requestData, 1000);
-
+  stream.on('data', onData);
   function onData(data) { connection.send(data); }
   
-  function onClose() {
-    logan.stdout.removeListener('data', onData);
-
-    if (logan.stdout.listeners('data').length === 0) {
-      clearInterval(interval);
-      interval = undefined;
-    }
-  }
+  connection.on('close', onClose);
+  function onClose() { stream.removeListener('data', onData); }
 }
 
 server.listen(8800);
