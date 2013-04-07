3ee5ec2dbfea57b6bb6a97d1c50401fba2b7e7e8
Luis Reis <luis.m.reis@gmail.com>
2013-04-07 03:31:05 +0100

    Add SPI decoder.

--- public/logan.js
+++ public/logan.js
@@ -10,7 +10,16 @@
       { label: 'SCK'   , bit: 22, theme: 'blue'   }, // 11,
       { label: 'R̅E̅S̅E̅T̅' , bit: 27, theme: 'red'    }, // 25,
       { label: 'BUTTON', bit: 24, theme: 'gray'   }
-    ]
+    ],
+    spiDecode : {
+      pinMap : {
+        'mosi' : 'MOSI',
+        'miso' : undefined,
+        'ss'   : 'C̅S̅',
+        'sclk' : 'SCK'
+      },
+      wordSize : 9
+    }
   };
 
   var data, chart;
@@ -27,6 +36,10 @@
       p.className = line.theme;
       lines.appendChild(p);
     });
+
+    var a = document.getElementById('download');
+    a.download = 'config.json'; // Chrome only (for now)
+    a.href = 'data:application/json;' + JSON.stringify(config);
   }
 
   setup();
@@ -66,7 +79,7 @@
       var columns = line.split('\t');
       if (columns.length !== 2) { return; }
 
-      var currentTick   = columns.shift();
+      var currentTick   = parseInt(columns.shift(), 10);
       var currentPinout = parseInt(columns.shift(), 16);
       if (currentTick === 0) {
         previousPinout = currentPinout;
@@ -92,6 +105,106 @@
           .y(function(d) { return d[1]; }));
     });
   };
+  
+  var spiDecode = global.spiDecode = function () {
+    var tsv = document.getElementById('tsv').value;
+    var data = document.querySelector('#spi .data');
+    data.innerHTML = '';
+
+    var pinMap = config.lines.reduce(function (map, line, index) {
+      map[line.label] = index;
+      return map;
+    }, {});
+    
+    var ssBit   = 1 << config.lines[pinMap[config.spiDecode.pinMap.ss  ]].bit;
+    var sclkBit = 1 << config.lines[pinMap[config.spiDecode.pinMap.sclk]].bit;
+    // var misoBit = 1 << config.lines[pinMap[config.spiDecode.pinMap.miso]].bit;
+    var mosiBit = 1 << config.lines[pinMap[config.spiDecode.pinMap.mosi]].bit;
+
+    var wordSizeBits = config.spiDecode.wordSize;
+    function binaryWord(currentWord, bitCount) {
+      var word = [];
+      if (bitCount !== 0) {
+        var bit;
+        for (bit = 1 << (bitCount - 1); bit; bit >>= 1) {
+          word.push((currentWord & bit) ? '1' : '0');
+        }
+      }
+      while (word.length < wordSizeBits) {
+        word.unshift('-');
+      }
+      return word.join('');
+    }
+
+    var wordSizeNibbles = Math.floor((config.spiDecode.wordSize - 1) / 4) + 1;
+    function hexWord(currentWord, bitCount) {
+      var word = currentWord.toString(16).split('');
+      while (word.length < bitCount / 4) {
+        word.unshift('0');
+      }
+      while (word.length < wordSizeNibbles) {
+        word.unshift('-');
+      }
+      return word.join('');
+    }
+
+    var previousPinout;
+    var currentWord = 0;
+    var bitCount = 0;
+    var words = [];
+    tsv.split('\n').forEach(function (line, lineNumber) {
+      var edge = false;
+      var columns = line.split('\t');
+      if (columns.length !== 2) { return; }
+
+      var currentTick   = parseInt(columns.shift(), 10);
+      var currentPinout = parseInt(columns.shift(), 16);
+      if (currentTick === 0) {
+        previousPinout = currentPinout;
+        return;
+      }
+
+      if ((currentPinout & ssBit) === 0 && (previousPinout & ssBit) !== 0) {
+        // ssBit went low => start new word
+        currentWord = 0;
+        bitCount = 0;
+      }
+
+      if ((currentPinout & ssBit) === 0) {
+        edge = (currentPinout & sclkBit) !== 0 && (previousPinout & sclkBit) === 0;
+        if (edge) {
+          // ascending edge
+          currentWord <<= 1;
+          if ((currentPinout & mosiBit) !== 0) currentWord |= 0x01;
+          bitCount++;
+        }
+      }
+
+      if ((currentPinout & ssBit) !== 0 && (previousPinout & ssBit) === 0) {
+        // ssBit went high => print current word
+        var bin = binaryWord(currentWord, bitCount);
+        var hex = hexWord(currentWord, bitCount);
+
+        if (edge) {
+          bin = bin.substring(0, bin.length) + '?';
+          hex = hex.substring(0, hex.length) + '?';
+        }
+
+        var word = bin + ' - ' + hex;
+
+        if (bitCount !== config.spiDecode.wordSize) {
+          word = '<span class="error">' + word + '</span>';
+        }
+
+        words.push(word);
+      }
+
+      previousPinout = currentPinout;
+    });
+
+    data.innerHTML = words.join('\n');
+  };
+  
 
   var buffer = [];
 
